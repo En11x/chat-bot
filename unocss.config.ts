4894f2ddc9f5b9bd1a8cbf065c389c665ca7666a
pp <pijiaxin@wsdashi.com>
2023-03-27 16:57:52 +0800

    feat: header

---
+++ unocss.config.ts
@@ -0,0 +1,30 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+} from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.1,
+      cdn: 'https://esm.sh/',
+    }),
+    presetTypography({
+      cssExtend: {
+        'ul,ol': {
+          'padding-left': '2.25em',
+          position: 'relative',
+        },
+      },
+    }),
+  ],
+  shortcuts:[{
+    'fc':'flex justify-center',
+    'fb': 'flex justify-between'
+  }]
+})
