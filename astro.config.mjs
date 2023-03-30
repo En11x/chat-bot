ea5cb28a7199dd2833185d24b74362988244129f
pp <pijiaxin@wsdashi.com>
2023-03-30 10:39:19 +0800

    feat: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import UnoCSS from 'unocss/astro'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react(), UnoCSS()],
+  output: 'server',
+  adapter: vercel(),
 })
